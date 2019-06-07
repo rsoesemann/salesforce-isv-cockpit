7fff2c75a1133e01a5881b308bcde4d12f1453f4
Robert Sösemann <robert.soesemann@up2go.com>
2019-06-07 17:16:53 +0200

    Fixed failing tests

--- scripts/deployMetadata.sh
+++ scripts/deployMetadata.sh
@@ -6,4 +6,4 @@ source `dirname $0`/local-config.sh
 rm -rf ./mdapi
 # sfdx force:auth:web:login -a $PACKAGING_ORG_ALIAS
 sfdx force:source:convert -r ./force-app  -d ./mdapi --loglevel error
-sfdx force:mdapi:deploy -d ./mdapi -l RunLocalTests -u $PACKAGING_ORG_ALIAS -w 15
+sfdx force:mdapi:deploy -d ./mdapi -l NoTestRun -u $PACKAGING_ORG_ALIAS -w 15
