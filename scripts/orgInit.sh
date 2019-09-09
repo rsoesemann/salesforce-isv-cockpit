3a71b50ce5a279fe6aa5350e8fa686edafeb7445
Robert Sösemann <robert.soesemann@logicline.de>
2019-09-09 17:45:17 +0200

    Rename Config to Setup to bettter match external lib

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -7,7 +7,7 @@ sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS -v $DEV_HUB_ALIAS
 sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f config/project-scratch-def.json -d 29
 sfdx force:package:install --package 04t30000001DWL0AAO --publishwait 5 --wait 10 -u $SCRATCH_ORG_ALIAS
 sfdx force:source:push
-sfdx force:user:permset:assign -n AppErrorAdmin
+sfdx force:user:permset:assign -n AppCockpitAdmin
 sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US" -u $SCRATCH_ORG_ALIAS
 sfdx force:apex:execute -f scripts/createSampleData.apex -u $SCRATCH_ORG_ALIAS
 sfdx force:org:open
