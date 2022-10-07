4124571a91c538c01001b7b22b994de3d98f90f1
Robert Sösemann <robert@soesemann.com>
2022-10-07 13:04:40 +0200

    Added more native (and Beta) Security features

--- scripts/createScratchOrg.sh
+++ scripts/createScratchOrg.sh
@@ -8,14 +8,13 @@ execute() {
 if [ -z "$secrets.DEV_HUB_URL" ]; then
   echo "set default devhub user"
   execute sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
-
-  echo "deleting old scratch org"
-  sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
 fi
 
+echo "Deleting old scratch org"
+sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
 
 echo "Creating scratch ORG"
-execute sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 7
+execute sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/scratch-org-def.json -d 7
 
 echo "Install dependencies"
 execute sfdx force:package:install --package 04t30000001DWL0AAO --publishwait 5 --wait 10 -u $SCRATCH_ORG_ALIAS
